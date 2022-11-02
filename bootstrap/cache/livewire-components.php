04cff6e518c277b81bebea0915876888f0a8a8e8
nikunjhj <42550307+nikunjhj@users.noreply.github.com>
2022-11-02 03:27:31 -0700

    Add files via upload
    committed files

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'admin.admin-add-product-component' => 'App\\Http\\Livewire\\Admin\\AdminAddProductComponent',
+  'admin.admin-product-component' => 'App\\Http\\Livewire\\Admin\\AdminProductComponent',
+  'cart-list' => 'App\\Http\\Livewire\\CartList',
+  'cart-update' => 'App\\Http\\Livewire\\CartUpdate',
+);
